06cce46c8c595d98de236e322f125e600b2e5ea5
Bisenteju3822 <111217510+Bisenteju3822@users.noreply.github.com>
2025-02-05 22:28:37 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,99 +1,35 @@
-import { useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState({ name: "", email: "" });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    switch (name) {
-      case "name":
-        setName(value);
-        setError((prevError) => ({
-          ...prevError,
-          name:
-            value.length < 3 ? "Name must be at least 3 characters long" : "",
-        }));
-        break;
-
-      case "email":
-        setEmail(value);
-        setError((prevError) => ({
-          ...prevError,
-          email: !/\S+@\S+\.\S+/.test(value) ? "Email is invalid" : "",
-        }));
-        break;
-
-      case "password":
-        setPassword(value);
-        break;
-
-      default:
-        break;
-    }
-
-    console.log({ name, value, error });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({ name, email, password, error });
-    // Your form submission logic here
-    if (!error.name && !error.email) {
-      alert("Form submitted successfully!");
-    } else {
-      alert("Please correct the errors before submitting.");
-    }
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={name}
-          onChange={handleChange}
-        />
-        {error.name && <p style={{ color: "red" }}>{error.name}</p>}
-
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-        {error.email && <p style={{ color: "red" }}>{error.email}</p>}
-
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
